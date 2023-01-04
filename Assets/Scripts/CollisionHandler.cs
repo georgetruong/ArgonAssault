fa1bdd7ad2382cb39040a6b2ad76555a82d1e67c
George Truong <truong.george@gmail.com>
2023-01-04 09:47:23 -0800

    Disable controls and reload scene after player collision

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,14 +1,24 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class CollisionHandler : MonoBehaviour
-{
-    void OnCollisionEnter(Collision other) {
-        Debug.Log(this.name + " collided with " + other.gameObject.name);
-    }
+public class CollisionHandler : MonoBehaviour {
+    [SerializeField] float loadDelay = 1f;
 
     void OnTriggerEnter(Collider other) {
-        Debug.Log($"{this.name} triggered by {other.gameObject.name}");
+        StartCrashSequence();
     }
+
+    void StartCrashSequence() {
+        GetComponent<PlayerControls>().enabled = false;
+        Invoke("ReloadLevel", loadDelay);
+    }
+
+    void ReloadLevel() {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
 }
