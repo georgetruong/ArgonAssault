3a06057dff4cc14cc8c2b47caebe9b205de6e79f
George Truong <truong.george@gmail.com>
2023-01-04 16:15:35 -0800

    Process hit and increase score

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,28 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathVFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 100;
+
+    Scoreboard scoreBoard;
+
+    void Start() {
+        // FindObjectOfType can be resource heavy. Okay in Start(), 
+        // but shouldn't use in Update() when it runs each frame.
+        scoreBoard = FindObjectOfType<Scoreboard>();    
+    }
 
     void OnParticleCollision(GameObject other) {
+        ProcessHit();
+        KillEnemy();
+    }
+
+    void KillEnemy() {
         GameObject vfx = Instantiate(deathVFX, transform.position, Quaternion.identity);
         vfx.transform.parent = parent;
         Destroy(this.gameObject);
+    }
+
+    void ProcessHit() {
+        scoreBoard.IncreaseScore(scorePerHit);
     }
 }
